6631315aae778f5e48ab5006d8127ff37bf84ce8
maraya28 <matiasaraya28@gmail.com>
2019-07-25 00:00:55 -0300

    Add Business Entity

---
+++ Models/ScheduleEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GymWebAPI.Models
+{
+    public class ScheduleEntity 
+    {
+        public ScheduleEntity(DayOfWeek day, int hours, int minutes)
+        {
+            Day = day;
+            Hour = new TimeSpan(hours, minutes, 0);
+        }
+
+        public DayOfWeek Day { get; private set; }
+        public TimeSpan Hour { get; private set; }
+        public string Hourformat => $"{Hour.Hours} : {Hour.Minutes.ToString()} Hs";
+    }
+}
